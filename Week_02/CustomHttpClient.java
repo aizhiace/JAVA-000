ed53f56cacbcba583d8827f236f86c20742d8756
aizhiace <2369365641@qq.com>
2020-10-28 15:32:16 +0800

    czh week02 homework

---
+++ Week_02/CustomHttpClient.java
@@ -0,0 +1,51 @@
+package com.czh.geek.homework.nio;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @author Caizh
+ * @date 2020/10/27
+ */
+public class CustomHttpClient {
+
+    public static void main(String[] args) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建http GET请求
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        CloseableHttpResponse response = null;
+        try {
+            // 执行请求
+            response = httpclient.execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
+                // 打印响应信息
+                System.out.println(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 相当于关闭浏览器
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
